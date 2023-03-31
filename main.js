8d4a39b310152ef8f765be003f98cade5095d033
Andriikryl <andreyk557@gmail.com>
2023-03-31 22:51:06 +0300

    feat[three/light]

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ const geometry = new THREE.SphereGeometry(3, 64, 64);
 
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.5,
+  metalness: 0.2,
 });
 
 const mesh = new THREE.Mesh(geometry, material);
@@ -19,6 +21,7 @@ scene.add(mesh);
 
 const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 10, 10);
+light.intensity = 1.25;
 scene.add(light);
 
 //Sizes
@@ -71,3 +74,32 @@ const tl = gsap.timeline({ defaults: { duration: 1 } });
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
 tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
 tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
+
+// mouse animation
+
+let mouseDown = false;
+let rgb = [];
+window.addEventListener("mousedown", () => {
+  mouseDown = true;
+});
+
+window.addEventListener("mouseuo", () => {
+  mouseDown = false;
+});
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width) * 255),
+      Math.round((e.pageX / sizes.height) * 255),
+      150,
+    ];
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`);
+
+    gsap.to(mesh.material.color, {
+      r: newColor.r,
+      g: newColor.g,
+      b: newColor.b,
+    });
+  }
+});
