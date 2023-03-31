f49886dbb99a86554d2515d0aa4813d29c08f734
Andriikryl <andreyk557@gmail.com>
2023-03-31 22:13:56 +0300

    feat[resizing/window]

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import "./style.css";
 
 const scene = new THREE.Scene();
 
@@ -16,9 +17,20 @@ const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 10, 10);
 scene.add(light);
 
+//Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 // camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100);
-camera.position.z = 52;
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.z = 20;
 scene.add(camera);
 
 // renderer
@@ -26,5 +38,21 @@ scene.add(camera);
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
 
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+//Resize
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
