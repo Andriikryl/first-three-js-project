b9cf3f7252ce43bce723fd86be2c082a3b6a6310
Andriikryl <andreyk557@gmail.com>
2023-03-31 22:38:07 +0300

    feat[simple/gsap-animation]

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "./style.css";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
+import { gsap } from "gsap";
 
 const scene = new THREE.Scene();
 
@@ -65,3 +66,8 @@ const loop = () => {
   window.requestAnimationFrame(loop);
 };
 loop();
+
+const tl = gsap.timeline({ defaults: { duration: 1 } });
+tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
+tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
+tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
