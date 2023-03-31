a720aed5f376051fc029d18766d2811475e400ab
Andriikryl <andreyk557@gmail.com>
2023-03-31 22:02:47 +0300

    feat[basic/scene]

--- main.js
+++ main.js
@@ -4,16 +4,21 @@ const scene = new THREE.Scene();
 
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83",
 });
 
 const mesh = new THREE.Mesh(geometry, material);
 
 scene.add(mesh);
 
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
+
 // camera
-const camera = new THREE.PerspectiveCamera(45, 800, 600);
+const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100);
+camera.position.z = 52;
 scene.add(camera);
 
 // renderer
